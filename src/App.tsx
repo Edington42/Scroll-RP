0f1ff53455d46aa702ed7f53ecaf4c3c3a6f24b0
Edington42 <47201937+Edington42@users.noreply.github.com>
2022-04-25 21:51:58 -0400

    Adding material UI and updating text.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello this is a new game.</h1>
+      <h2>Stuff will be here </h2>
     </div>
   );
 }
